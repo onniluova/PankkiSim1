0da83b1f3c9285267c82fc9c9ec0f924a2eac501
Jannassl <Jan.nassling@metropolia.fi>
2024-03-18 14:37:57 +0200

    Javadoc komentteja

---
+++ src/main/resources/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controller"},{"l":"dao"},{"l":"datasource"},{"l":"eduni.distributions"},{"l":"Entity"},{"l":"simu.framework"},{"l":"simu.model"},{"l":"view"}];updateSearchResults();
