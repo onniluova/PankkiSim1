9a758de4042ff2466f6a702c641722a03d769873
Jannassl <Jan.nassling@metropolia.fi>
2024-03-19 00:35:07 +0200

    Saapuneet asiakkaat ja palvelluksi tulleet asiakkaat

--- src/main/resources/Kyselyt.sql
+++ src/main/resources/Kyselyt.sql
@@ -2,11 +2,17 @@ DROP DATABASE IF EXISTS simu_database;
 CREATE DATABASE simu_database;
 USE simu_database;
 
-CREATE TABLE Tulokset (
+CREATE TABLE tulokset (
     id INT NOT NULL AUTO_INCREMENT,
     kokonaisaika DECIMAL(10.2) NOT NULL,
     asiakkaiden_maara INT(10) NOT NULL,
     asiakkaiden_keskimaarainen_ika DECIMAL(10, 2) NOT NULL,
     PRIMARY KEY (id)
 );
+
+alter table tulokset add palvelupisteen_palveluaika decimal(10,2);
+
+alter table tulokset add suoritusteho decimal(10.2);
+ALTER TABLE tulokset ADD saapuneet_asiakkaat INT(6);
+ALTER TABLE tulokset ADD palvellut_asiakkaat INT(6);
 
